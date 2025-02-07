4de946ff6c24fea0fb6c7725f73598a591306cd1
godzu2208 <lel54879@gmail.com>
2025-02-08 00:01:19 +0700

    bai 2

--- helloworld.js
+++ helloworld.js
@@ -1,2 +1,17 @@
-console.log('Hello World1111');
-alert("Hello World from file html");
+var handleClickOK = function() {
+    var name = document.getElementById('inputName').value
+    document.getElementById('hello_name').innerHTML = name
+    // document.write('Hello ' + name + '!')
+}
+
+// handleClickOK()
+
+var thong_bao = function() {
+    document.getElementById('thong_bao').innerHTML += "Timeout!<br>"
+}
+setTimeout(thong_bao, 3000)
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('thong_bao').innerHTML += "Timeousdfdsfdsft!<br>";
+});
+// document.getElementById("test").innerHTML += "Waiting!<br>"
+// document.getElementById('test').innerHTML += "Timadsadsaeout!<br>"
