a1f99a1b892ed57f25e6b551539d1a818113ce0d
godzu2208 <lel54879@gmail.com>
2025-02-08 10:56:56 +0700

    bai3

---
+++ bai2.js
@@ -0,0 +1,13 @@
+let x = 10;
+let y = 20;
+let z = 30;
+
+console.log("Test String :", "x = ",x, "y = ",y, "z = ",z)
+
+let a = 20;
+let b = 30;
+let c = 40;
+
+
+
+console.log("Test String :", "a= ",a, "b = ",b, "c = ",c)
--- helloworld.js
+++ helloworld.js
@@ -1,17 +1,44 @@
-var handleClickOK = function() {
-    var name = document.getElementById('inputName').value
-    document.getElementById('hello_name').innerHTML = name
-    // document.write('Hello ' + name + '!')
-}
+// var handleClickOK = function() {
+//     var name = document.getElementById('inputName').value
+//     document.getElementById('hello_name').innerHTML = name
+//     // document.write('Hello ' + name + '!')
+// }
 
-// handleClickOK()
+// // handleClickOK()
 
-var thong_bao = function() {
-    document.getElementById('thong_bao').innerHTML += "Timeout!<br>"
+// var thong_bao = function() {
+//     document.getElementById('thong_bao').innerHTML += "Timeout!<br>"
+// }
+// setTimeout(thong_bao, 3000)
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.getElementById('thong_bao').innerHTML += "Timeousdfdsfdsft!<br>";
+// });
+// // document.getElementById("test").innerHTML += "Waiting!<br>"
+// // document.getElementById('test').innerHTML += "Timadsadsaeout!<br>"
+
+// let a = 10;
+
+// var print_a = function() {
+//     document.write(a)
+// }
+
+function exampleVar() {
+    if (true) {
+      var x = 10; // Biến x có phạm vi trong hàm exampleVar
+      console.log(x); // 10
+    }
+    console.log(x); // 10 (vẫn truy cập được vì var không có block scope)
+  }
+  exampleVar();
+//   console.log(x); // ReferenceError: x is not defined (x không tồn tại bên ngoài hàm)
+
+function exampleLet() {
+    // let y;
+    if (true) {
+        let y = 20; // Biến y có phạm vi trong khối if
+        console.log(y); // 20
+      }
+      console.log(y); // ReferenceError: y is not defined (y không tồn tại bên ngoài khối if)
 }
-setTimeout(thong_bao, 3000)
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('thong_bao').innerHTML += "Timeousdfdsfdsft!<br>";
-});
-// document.getElementById("test").innerHTML += "Waiting!<br>"
-// document.getElementById('test').innerHTML += "Timadsadsaeout!<br>"
+exampleLet();
+// console.log(y);
