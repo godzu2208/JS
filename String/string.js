6541ec674c0408f2012fdcffce8e6ce926d6b627
godzu2208 <lel54879@gmail.com>
2025-02-13 00:12:50 +0700

    number

--- String/string.js
+++ String/string.js
@@ -5,6 +5,12 @@ let stringS = `I'm Developer "Web"`
 let testS = s.split(" ");
 console.log(testS);
 
+let testS2 = s.split(" ", 2);
+console.log(testS2);
+
+// let testS3 = s.split(" ", 3);
+// console.log(testS3);
+
 
 
 
---
+++ assets/js/number.js
@@ -0,0 +1,20 @@
+console.log("Hello World".toUpperCase());
+console.log("Tôi tên là Lê Hồng Lĩnh".toUpperCase());
+let a = 10;
+let b = 20;
+let c = 10.2;
+let d = '12';
+
+console.log("a + d = ",a + d,"|", "a = ",typeof a,"|" ,"d = ",typeof d);
+console.log("a * d = ",a*d, "|", "a = ",typeof a, "|","d = ",typeof d);
+
+
+let z = +10; // Convert String to Number
+console.log("z = ",z, "|", "z = ",typeof z);
+// Trong JS, các kiểu dữ liệu luôn cố gắng convert sang kiểu dữ liệu số để thực hiện phép tính
+
+
+function displayDate() {
+    console.log(Date());
+    document.getElementById('test').innerHTML = Date();
+}
